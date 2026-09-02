379b01a1858a4d35a9659543ac178d22aa021914
fjaakyou <kevin.dehabe@gmx.de>
2026-09-02 18:41:32 +0200

    Use individual Tap Battle production server

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 'use strict';
 
 // One-process production entrypoint. The stable 4.5 core is extended in-memory
-// with the tested v4.6 browser-party features (minigames, invites, master handoff).
-require('./server-v46');
+// with the tested browser-party features (minigames, invites, master handoff).
+require('./server-v461');
